36b1c333ef9986fb0696ef81b75a292d4d9e576c
Alpha Dragon <admin@backendforge.com>
2023-08-29 09:38:23 +0200

    add tile preprocessor

--- src/controlnet_aux/processor.py
+++ src/controlnet_aux/processor.py
@@ -7,72 +7,97 @@ from typing import Dict, Optional, Union
 
 from PIL import Image
 
-from controlnet_aux import (CannyDetector, ContentShuffleDetector, HEDdetector,
-                            LeresDetector, LineartAnimeDetector,
-                            LineartDetector, MediapipeFaceDetector,
-                            MidasDetector, MLSDdetector, NormalBaeDetector,
-                            OpenposeDetector, PidiNetDetector, ZoeDetector)
+from controlnet_aux import (
+    CannyDetector,
+    ContentShuffleDetector,
+    HEDdetector,
+    LeresDetector,
+    LineartAnimeDetector,
+    LineartDetector,
+    MediapipeFaceDetector,
+    MidasDetector,
+    MLSDdetector,
+    NormalBaeDetector,
+    OpenposeDetector,
+    PidiNetDetector,
+    ZoeDetector,
+    TileDetector,
+)
 
 LOGGER = logging.getLogger(__name__)
 
 
 MODELS = {
     # checkpoint models
-    'scribble_hed': {'class': HEDdetector, 'checkpoint': True},
-    'softedge_hed': {'class': HEDdetector, 'checkpoint': True},
-    'scribble_hedsafe': {'class': HEDdetector, 'checkpoint': True},
-    'softedge_hedsafe': {'class': HEDdetector, 'checkpoint': True},
-    'depth_midas': {'class': MidasDetector, 'checkpoint': True},
-    'mlsd': {'class': MLSDdetector, 'checkpoint': True},
-    'openpose': {'class': OpenposeDetector, 'checkpoint': True},
-    'openpose_face': {'class': OpenposeDetector, 'checkpoint': True},
-    'openpose_faceonly': {'class': OpenposeDetector, 'checkpoint': True},
-    'openpose_full': {'class': OpenposeDetector, 'checkpoint': True},
-    'openpose_hand': {'class': OpenposeDetector, 'checkpoint': True},
-    'scribble_pidinet': {'class': PidiNetDetector, 'checkpoint': True},
-    'softedge_pidinet': {'class': PidiNetDetector, 'checkpoint': True},
-    'scribble_pidsafe': {'class': PidiNetDetector, 'checkpoint': True},
-    'softedge_pidsafe': {'class': PidiNetDetector, 'checkpoint': True},
-    'normal_bae': {'class': NormalBaeDetector, 'checkpoint': True},
-    'lineart_coarse': {'class': LineartDetector, 'checkpoint': True},
-    'lineart_realistic': {'class': LineartDetector, 'checkpoint': True},
-    'lineart_anime': {'class': LineartAnimeDetector, 'checkpoint': True},
-    'depth_zoe': {'class': ZoeDetector, 'checkpoint': True}, 
-    'depth_leres': {'class': LeresDetector, 'checkpoint': True}, 
-    'depth_leres++': {'class': LeresDetector, 'checkpoint': True}, 
+    "scribble_hed": {"class": HEDdetector, "checkpoint": True},
+    "softedge_hed": {"class": HEDdetector, "checkpoint": True},
+    "scribble_hedsafe": {"class": HEDdetector, "checkpoint": True},
+    "softedge_hedsafe": {"class": HEDdetector, "checkpoint": True},
+    "depth_midas": {"class": MidasDetector, "checkpoint": True},
+    "mlsd": {"class": MLSDdetector, "checkpoint": True},
+    "openpose": {"class": OpenposeDetector, "checkpoint": True},
+    "openpose_face": {"class": OpenposeDetector, "checkpoint": True},
+    "openpose_faceonly": {"class": OpenposeDetector, "checkpoint": True},
+    "openpose_full": {"class": OpenposeDetector, "checkpoint": True},
+    "openpose_hand": {"class": OpenposeDetector, "checkpoint": True},
+    "scribble_pidinet": {"class": PidiNetDetector, "checkpoint": True},
+    "softedge_pidinet": {"class": PidiNetDetector, "checkpoint": True},
+    "scribble_pidsafe": {"class": PidiNetDetector, "checkpoint": True},
+    "softedge_pidsafe": {"class": PidiNetDetector, "checkpoint": True},
+    "normal_bae": {"class": NormalBaeDetector, "checkpoint": True},
+    "lineart_coarse": {"class": LineartDetector, "checkpoint": True},
+    "lineart_realistic": {"class": LineartDetector, "checkpoint": True},
+    "lineart_anime": {"class": LineartAnimeDetector, "checkpoint": True},
+    "depth_zoe": {"class": ZoeDetector, "checkpoint": True},
+    "depth_leres": {"class": LeresDetector, "checkpoint": True},
+    "depth_leres++": {"class": LeresDetector, "checkpoint": True},
     # instantiate
-    'shuffle': {'class': ContentShuffleDetector, 'checkpoint': False},
-    'mediapipe_face': {'class': MediapipeFaceDetector, 'checkpoint': False},
-    'canny': {'class': CannyDetector, 'checkpoint': False},
+    "shuffle": {"class": ContentShuffleDetector, "checkpoint": False},
+    "mediapipe_face": {"class": MediapipeFaceDetector, "checkpoint": False},
+    "canny": {"class": CannyDetector, "checkpoint": False},
+    "tile": {"class": TileDetector, "checkpoint": False},
 }
 
 
 MODEL_PARAMS = {
-    'scribble_hed': {'scribble': True},
-    'softedge_hed': {'scribble': False},
-    'scribble_hedsafe': {'scribble': True, 'safe': True},
-    'softedge_hedsafe': {'scribble': False, 'safe': True},
-    'depth_midas': {},
-    'mlsd': {},
-    'openpose': {'include_body': True, 'include_hand': False, 'include_face': False},
-    'openpose_face': {'include_body': True, 'include_hand': False, 'include_face': True},
-    'openpose_faceonly': {'include_body': False, 'include_hand': False, 'include_face': True},
-    'openpose_full': {'include_body': True, 'include_hand': True, 'include_face': True},
-    'openpose_hand': {'include_body': False, 'include_hand': True, 'include_face': False},
-    'scribble_pidinet': {'safe': False, 'scribble': True},
-    'softedge_pidinet': {'safe': False, 'scribble': False},
-    'scribble_pidsafe': {'safe': True, 'scribble': True},
-    'softedge_pidsafe': {'safe': True, 'scribble': False},
-    'normal_bae': {},
-    'lineart_realistic': {'coarse': False},
-    'lineart_coarse': {'coarse': True},
-    'lineart_anime': {},
-    'canny': {},
-    'shuffle': {},
-    'depth_zoe': {},
-    'depth_leres': {'boost': False},
-    'depth_leres++': {'boost': True},
-    'mediapipe_face': {},
+    "scribble_hed": {"scribble": True},
+    "softedge_hed": {"scribble": False},
+    "scribble_hedsafe": {"scribble": True, "safe": True},
+    "softedge_hedsafe": {"scribble": False, "safe": True},
+    "depth_midas": {},
+    "mlsd": {},
+    "openpose": {"include_body": True, "include_hand": False, "include_face": False},
+    "openpose_face": {
+        "include_body": True,
+        "include_hand": False,
+        "include_face": True,
+    },
+    "openpose_faceonly": {
+        "include_body": False,
+        "include_hand": False,
+        "include_face": True,
+    },
+    "openpose_full": {"include_body": True, "include_hand": True, "include_face": True},
+    "openpose_hand": {
+        "include_body": False,
+        "include_hand": True,
+        "include_face": False,
+    },
+    "scribble_pidinet": {"safe": False, "scribble": True},
+    "softedge_pidinet": {"safe": False, "scribble": False},
+    "scribble_pidsafe": {"safe": True, "scribble": True},
+    "softedge_pidsafe": {"safe": True, "scribble": False},
+    "normal_bae": {},
+    "lineart_realistic": {"coarse": False},
+    "lineart_coarse": {"coarse": True},
+    "lineart_anime": {},
+    "canny": {},
+    "shuffle": {},
+    "depth_zoe": {},
+    "depth_leres": {"boost": False},
+    "depth_leres++": {"boost": True},
+    "mediapipe_face": {},
+    "tile": {},
 }
 
 CHOICES = f"Choices for the processor are {list(MODELS.keys())}"
@@ -85,13 +110,15 @@ class Processor:
         Args:
             processor_id (str): processor name, options are 'hed, midas, mlsd, openpose,
                                 pidinet, normalbae, lineart, lineart_coarse, lineart_anime,
-                                canny, content_shuffle, zoe, mediapipe_face
+                                canny, content_shuffle, zoe, mediapipe_face, tile'
             params (Optional[Dict]): parameters for the processor
         """
         LOGGER.info("Loading %s".format(processor_id))
 
         if processor_id not in MODELS:
-            raise ValueError(f"{processor_id} is not a valid processor id. Please make sure to choose one of {', '.join(MODELS.keys())}")
+            raise ValueError(
+                f"{processor_id} is not a valid processor id. Please make sure to choose one of {', '.join(MODELS.keys())}"
+            )
 
         self.processor_id = processor_id
         self.processor = self.load_processor(self.processor_id)
@@ -102,7 +129,7 @@ class Processor:
         if params:
             self.params.update(params)
 
-    def load_processor(self, processor_id: str) -> 'Processor':
+    def load_processor(self, processor_id: str) -> "Processor":
         """Load controlnet aux processors
 
         Args:
@@ -111,17 +138,18 @@ class Processor:
         Returns:
             Processor: controlnet aux processor
         """
-        processor = MODELS[processor_id]['class']
+        processor = MODELS[processor_id]["class"]
 
         # check if the proecssor is a checkpoint model
-        if MODELS[processor_id]['checkpoint']:
+        if MODELS[processor_id]["checkpoint"]:
             processor = processor.from_pretrained("lllyasviel/Annotators")
         else:
             processor = processor()
         return processor
 
-    def __call__(self, image: Union[Image.Image, bytes],
-                 to_pil: bool = True) -> Union[Image.Image, bytes]:
+    def __call__(
+        self, image: Union[Image.Image, bytes], to_pil: bool = True
+    ) -> Union[Image.Image, bytes]:
         """processes an image with a controlnet aux processor
 
         Args:
@@ -141,5 +169,5 @@ class Processor:
             return processed_image
         else:
             output_bytes = io.BytesIO()
-            processed_image.save(output_bytes, format='JPEG')
+            processed_image.save(output_bytes, format="JPEG")
             return output_bytes.getvalue()
--- tests/test_controlnet_aux.py
+++ tests/test_controlnet_aux.py
@@ -7,28 +7,50 @@ import pytest
 import requests
 from PIL import Image
 
-from controlnet_aux import (CannyDetector, ContentShuffleDetector, HEDdetector,
-                            LeresDetector, LineartAnimeDetector,
-                            LineartDetector, MediapipeFaceDetector,
-                            MidasDetector, MLSDdetector, NormalBaeDetector,
-                            OpenposeDetector, PidiNetDetector, SamDetector,
-                            ZoeDetector)
+from controlnet_aux import (
+    CannyDetector,
+    ContentShuffleDetector,
+    HEDdetector,
+    LeresDetector,
+    LineartAnimeDetector,
+    LineartDetector,
+    MediapipeFaceDetector,
+    MidasDetector,
+    MLSDdetector,
+    NormalBaeDetector,
+    OpenposeDetector,
+    PidiNetDetector,
+    SamDetector,
+    ZoeDetector,
+    TileDetector,
+)
 
 OUTPUT_DIR = "tests/outputs"
+
 
 def output(name, img):
     img.save(os.path.join(OUTPUT_DIR, "{:s}.png".format(name)))
 
+
 def common(name, processor, img):
     output(name, processor(img))
     output(name + "_pil_np", Image.fromarray(processor(img, output_type="np")))
-    output(name + "_np_np", Image.fromarray(processor(np.array(img, dtype=np.uint8), output_type="np")))
-    output(name + "_np_pil", processor(np.array(img, dtype=np.uint8), output_type="pil"))
-    output(name + "_scaled", processor(img, detect_resolution=640, image_resolution=768))
+    output(
+        name + "_np_np",
+        Image.fromarray(processor(np.array(img, dtype=np.uint8), output_type="np")),
+    )
+    output(
+        name + "_np_pil", processor(np.array(img, dtype=np.uint8), output_type="pil")
+    )
+    output(
+        name + "_scaled", processor(img, detect_resolution=640, image_resolution=768)
+    )
+
 
 def return_pil(name, processor, img):
     output(name + "_pil_false", Image.fromarray(processor(img, return_pil=False)))
     output(name + "_pil_true", processor(img, return_pil=True))
+
 
 @pytest.fixture(scope="module")
 def img():
@@ -40,10 +62,12 @@ def img():
     img = Image.open(BytesIO(response.content)).convert("RGB").resize((512, 512))
     return img
 
+
 def test_canny(img):
     canny = CannyDetector()
     common("canny", canny, img)
     output("canny_img", canny(img=img))
+
 
 def test_hed(img):
     hed = HEDdetector.from_pretrained("lllyasviel/Annotators")
@@ -52,10 +76,12 @@ def test_hed(img):
     output("hed_safe", hed(img, safe=True))
     output("hed_scribble", hed(img, scribble=True))
 
+
 def test_leres(img):
     leres = LeresDetector.from_pretrained("lllyasviel/Annotators")
     common("leres", leres, img)
     output("leres_boost", leres(img, boost=True))
+
 
 def test_lineart(img):
     lineart = LineartDetector.from_pretrained("lllyasviel/Annotators")
@@ -63,30 +89,36 @@ def test_lineart(img):
     return_pil("lineart", lineart, img)
     output("lineart_coarse", lineart(img, coarse=True))
 
+
 def test_lineart_anime(img):
     lineart_anime = LineartAnimeDetector.from_pretrained("lllyasviel/Annotators")
     common("lineart_anime", lineart_anime, img)
     return_pil("lineart_anime", lineart_anime, img)
+
 
 def test_mediapipe_face(img):
     mediapipe = MediapipeFaceDetector()
     common("mediapipe", mediapipe, img)
     output("mediapipe_image", mediapipe(image=img))
 
+
 def test_midas(img):
     midas = MidasDetector.from_pretrained("lllyasviel/Annotators")
     common("midas", midas, img)
     output("midas_normal", midas(img, depth_and_normal=True)[1])
+
 
 def test_mlsd(img):
     mlsd = MLSDdetector.from_pretrained("lllyasviel/Annotators")
     common("mlsd", mlsd, img)
     return_pil("mlsd", mlsd, img)
 
+
 def test_normalbae(img):
     normal_bae = NormalBaeDetector.from_pretrained("lllyasviel/Annotators")
     common("normal_bae", normal_bae, img)
     return_pil("normal_bae", normal_bae, img)
+
 
 def test_openpose(img):
     openpose = OpenposeDetector.from_pretrained("lllyasviel/Annotators")
@@ -94,10 +126,23 @@ def test_openpose(img):
     return_pil("openpose", openpose, img)
     output("openpose_hand_and_face_false", openpose(img, hand_and_face=False))
     output("openpose_hand_and_face_true", openpose(img, hand_and_face=True))
-    output("openpose_face", openpose(img, include_body=True, include_hand=False, include_face=True))
-    output("openpose_faceonly", openpose(img, include_body=False, include_hand=False, include_face=True))
-    output("openpose_full", openpose(img, include_body=True, include_hand=True, include_face=True))
-    output("openpose_hand", openpose(img, include_body=True, include_hand=True, include_face=False))
+    output(
+        "openpose_face",
+        openpose(img, include_body=True, include_hand=False, include_face=True),
+    )
+    output(
+        "openpose_faceonly",
+        openpose(img, include_body=False, include_hand=False, include_face=True),
+    )
+    output(
+        "openpose_full",
+        openpose(img, include_body=True, include_hand=True, include_face=True),
+    )
+    output(
+        "openpose_hand",
+        openpose(img, include_body=True, include_hand=True, include_face=False),
+    )
+
 
 def test_pidi(img):
     pidi = PidiNetDetector.from_pretrained("lllyasviel/Annotators")
@@ -106,16 +151,27 @@ def test_pidi(img):
     output("pidi_safe", pidi(img, safe=True))
     output("pidi_scribble", pidi(img, scribble=True))
 
+
 def test_sam(img):
-    sam = SamDetector.from_pretrained("ybelkada/segment-anything", subfolder="checkpoints")
+    sam = SamDetector.from_pretrained(
+        "ybelkada/segment-anything", subfolder="checkpoints"
+    )
     common("sam", sam, img)
     output("sam_image", sam(image=img))
+
 
 def test_shuffle(img):
     shuffle = ContentShuffleDetector()
     common("shuffle", shuffle, img)
     return_pil("shuffle", shuffle, img)
 
+
 def test_zoe(img):
     zoe = ZoeDetector.from_pretrained("lllyasviel/Annotators")
     common("zoe", zoe, img)
+
+
+def test_tile(img):
+    tile = TileDetector()
+    common("tile", tile, img)
+    output("tile_img", tile(img))
